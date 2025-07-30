eb31e7b218131f0aaee3a54e08b9dd19f63f72b5
wawawoh <maahiraadmani@hotmail.com>
2025-07-30 12:55:17 +0100

    rmeoved

--- guess-the-type/src/Game.tsx
+++ guess-the-type/src/Game.tsx
@@ -1,4 +1,4 @@
-import React, {  useEffect, useState } from "react"
+import  {  useEffect, useState } from "react"
 import { chosenTypeContext } from "./chosenTypeContext"
 
 import TypeSection from "./TypeSection"
